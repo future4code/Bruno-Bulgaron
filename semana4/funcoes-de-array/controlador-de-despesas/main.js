4512491204eef6a6780ffaf7e5bf6a735c530e4b
Bruno Bulgaron <brunohbulgaron@gmail.com>
2020-03-12 13:57:52 -0300

    Etapa 1 completa.

---
+++ semana4/funcoes-de-array/controlador-de-despesas/main.js
@@ -0,0 +1,9 @@
+let campoValor = document.getElementById("valorDespesa");
+let campoTipo = document.getElementById("tipoDespesa");
+let campoDescricao = document.getElementById("descricaoDespesa");
+
+function addDespesa(){
+    campoValor.value = "";
+    campoTipo.value = "";
+    campoDescricao.value = "";
+}
