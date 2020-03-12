1a6f58983ef27806afa13e649714eb90a7ad56fd
Bruno Bulgaron <brunohbulgaron@gmail.com>
2020-03-12 16:55:26 -0300

    Etapa 4 incompleta.

--- semana4/funcoes-de-array/controlador-de-despesas/main.js
+++ semana4/funcoes-de-array/controlador-de-despesas/main.js
@@ -14,15 +14,54 @@ function addDespesa(){
     let campoTipo = document.getElementById("tipoDespesa");
     let campoDescricao = document.getElementById("descricaoDespesa");
 
-    const novaDespesa = new Despesa(campoValor.value, campoTipo.value, campoDescricao.value);
+    if(campoValor.value === "" || campoTipo.value === "" || campoDescricao.value === ""){
+        alert("Preencha os 3 campos!");
+    } else {
+        const novaDespesa = new Despesa(campoValor.value, campoTipo.value, campoDescricao.value);
 
-    arrayDeDespesa.push(novaDespesa);
-    
-    campoValor.value = "";
-    campoTipo.value = "";
-    campoDescricao.value = "";
+        arrayDeDespesa.push(novaDespesa);
+        
+        campoValor.value = "";
+        campoTipo.value = "";
+        campoDescricao.value = "";
 
-    console.log(arrayDeDespesa);
+        console.log(arrayDeDespesa);
+
+    }
 
 }
 
+
+
+function filtrarDespesas() {
+
+    if(valorMinimoDespesa.value === "" || valorMaximoDespesa.value === ""){
+        alert("Preencha os campos!");
+    } else {
+
+        const listaFiltrada = document.getElementById("listaFiltrada")
+        
+        let tipoDespesa = document.getElementById("tipoDespesa");
+        let valorMinimoDespesa = document.getElementById("valorMinimoDespesa");
+        let valorMaximoDespesa = document.getElementById("valorMaximoDespesa");
+                
+        let despesasFiltradas = arrayDeDespesa.filter
+        ((despesa) => {
+        return despesa.tipoDespesa === tipoDespesa && despesa.valores >= valorMinimoDespesa && despesa.valores <= valorMaximoDespesa
+    })
+    for(const elementos of despesasFiltradas) {
+
+    }
+
+    }
+   
+}
+
+
+function limparFiltros() {
+    let valorMinimoDespesa = document.getElementById("valorMinimoDespesa");
+    let valorMaximoDespesa = document.getElementById("valorMaximoDespesa");
+
+    valorMinimoDespesa.value = "";
+    valorMaximoDespesa.value = "";
+}
