f8310b2c87c473ad842a155c6fb2aab44cd31e7e
Bruno Bulgaron <brunohbulgaron@gmail.com>
2020-03-12 14:10:37 -0300

    Etapa 2 completa.

--- semana4/funcoes-de-array/controlador-de-despesas/main.js
+++ semana4/funcoes-de-array/controlador-de-despesas/main.js
@@ -1,9 +1,24 @@
-let campoValor = document.getElementById("valorDespesa");
-let campoTipo = document.getElementById("tipoDespesa");
-let campoDescricao = document.getElementById("descricaoDespesa");
+let campoValor = document.getElementById("valorDespesa").value;
+let campoTipo = document.getElementById("tipoDespesa").value;
+let campoDescricao = document.getElementById("descricaoDespesa").value;
+
+class Despesa {
+    constructor(valor, tipo, descricao) {
+        this.valor = valor;
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
+}
+
+let arrayDeDespesa = [campoValor, campoTipo, campoDescricao];
 
 function addDespesa(){
+    const novaDesepsa = new Despesa(arrayDeDespesa[0], arrayDeDespesa[1], arrayDeDespesa[2]);
+
     campoValor.value = "";
     campoTipo.value = "";
     campoDescricao.value = "";
+
+    console.log(arrayDeDespesa);
 }
+
