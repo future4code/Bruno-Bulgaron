1ca3b8945c177e0ffaa60076b3320cc5d0b1b790
Bruno Bulgaron <brunohbulgaron@gmail.com>
2020-04-16 16:54:25 -0300

    Parte inicial das alterações

--- semana9/astro-match/src/actions/profiles.js
+++ semana9/astro-match/src/actions/profiles.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
 export const clearSwipes = () => async (dispatch) => {
-	await axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/clear')
+	const body = 
+	
+	await axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/bruno/clear')
 }
